b9dce438e8f636908c854391c7912444336c729e
Bernardo Cassina Buerba <bernardocassinabuerba@Bernardos-MacBook-Pro.local>
2017-01-08 21:20:11 -0600

    Added .gitignore template and bower json also devserver script, refactor build.sh.

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 source  venv/bin/activate
 pip install -r requirements.txt -t lib
 pip install -r requirements.txt
-dart --out=game/static/dart.js game/dart/main.dart
+bower install
 compass compile
 deactivate
+bash devserver.sh
